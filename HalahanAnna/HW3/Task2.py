f29bc73e49eafe5d1cc1f839b360aff7737a0020
DinnoAnya <annahalahan09@gmail.com>
2024-01-24 01:38:10 +0200

    added HW3

--- HalahanAnna/HW3/Task2.py
+++ HalahanAnna/HW3/Task2.py
@@ -6,6 +6,10 @@ for digits in str(number):
 reverse = int(str(number)[::-1])
 ascending = int(''.join(sorted(str(number))))
 
+print(f"Product number: {digits}")
+print(f"Numbers in reverse order: {reverse}")
+print(f"Ascending numbers: {ascending}")
+
 print(f"Добуток цифр: {digits}")
 print(f"Число у зворотньому порядку: {reverse}")
 print(f"Число з відсортованими цифрами (за зростанням): {ascending}")
