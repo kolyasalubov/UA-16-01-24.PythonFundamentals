76d5a2df22ed2ad97bddc22e2a100993bfe69367
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-01-26 15:53:01 +0200

    some practice

--- PolinaChernyshova/HW3/task3.py
+++ PolinaChernyshova/HW3/task3.py
@@ -1,7 +1,10 @@
 # Interchange the values of two variables without using the third variable.
 
-first_num = -12
-second_num = 65
-print(f'first_num = {first_num}, second_num = {second_num} ')
-first_num, second_num = second_num, first_num
-print(f'first_num = {first_num}, second_num = {second_num} ')
+# first_num = -12
+# second_num = 65
+# print(f'first_num = {first_num}, second_num = {second_num} ')
+# first_num, second_num = second_num, first_num
+# print(f'first_num = {first_num}, second_num = {second_num} ')
+
+print("John" > "Jhon")
+print("Emma" < "Emm")
