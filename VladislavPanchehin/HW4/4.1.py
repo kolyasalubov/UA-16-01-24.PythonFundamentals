828f016cdf28dd55f3f5c7372875e2db40db491b
Vladislav Panchehin <vaduamediafox@gmail.com>
2024-01-28 00:28:07 +0200

    updated 4.1.py

--- VladislavPanchehin/HW4/4.1.py
+++ VladislavPanchehin/HW4/4.1.py
@@ -1,1 +1,7 @@
-﻿
+
+temperature = int(input("Enter the temperature in Celsius:"))
+converted_temperature = int((temperature * 9/5) + 32)
+if temperature <= -273.15:
+    print(f"Error: Temperature below absolute zero -273.15°C")
+else:
+    print(f"{temperature}°C is equivalent to {converted_temperature}F")
