0db9da44c28cd5c182db3f9361099e32a3e52df9
Olexandr_Hramatovych <sashahram08@gmail.com>
2024-01-29 18:16:33 +0200

    deleted folder's of other users from branch OlexanderHramatovych/HW4

--- PolinaChernyshova/HW3/task3.py
+++
@@ -1,7 +0,0 @@
-# Interchange the values of two variables without using the third variable.
-
-first_num = -12
-second_num = 65
-print(f'first_num = {first_num}, second_num = {second_num} ')
-first_num, second_num = second_num, first_num
-print(f'first_num = {first_num}, second_num = {second_num} ')
--- VolodymyrKulak/HW3/03.1 PRACTICAL TASKS--3.py
+++
@@ -1,8 +0,0 @@
-
-first = str(input("Type first variable: "))
-second = str(input("Type second variable: "))
-
-first, second = second, first
-
-print("First variable is now " + first)
-print("Second variable is now " + second)
