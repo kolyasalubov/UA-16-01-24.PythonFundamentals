60d49990a092ce9c54b6d7ddd263c0de2eeadff1
NataliiaZinchenko <nataellie@gmail.com>
2024-01-29 15:40:49 +0100

    added HW3 folder, nzinchenko_3.1.py and nzinchenko_3.2.py completed

---
+++ NataliiaZinchenko/HW3/nzinchenko_3.1.py
@@ -0,0 +1,89 @@
+zen_of_python = """1.Beautiful is better than ugly.
+2.Explicit is better than implicit.
+3.Simple is better than complex.
+4.Complex is better than complicated.
+5.Flat is better than nested.
+6.Sparse is better than dense.
+7.Readability counts.
+8.Special cases aren't special enough to break the rules.
+9.Although practicality beats purity.
+10.Errors should never pass silently.
+11.Unless explicitly silenced.
+12.In the face of ambiguity, refuse the temptation to guess.
+13.There should be one-- and preferably only one --obvious way to do it.
+14.Although that way may not be obvious at first unless you're Dutch.
+15.Now is better than never.
+16.Although never is often better than *right* now.
+17.If the implementation is hard to explain, it's a bad idea.
+18.If the implementation is easy to explain, it may be a good idea.
+19.Namespaces are one honking great idea -- let's do more of those!
+20.Beautiful is better than ugly.
+21.Explicit is better than implicit.
+22.Simple is better than complex.
+23.Complex is better than complicated.
+24.Flat is better than nested.
+25.Sparse is better than dense.
+26.Readability counts.
+27.Special cases aren't special enough to break the rules.
+28.Although practicality beats purity.
+29.Errors should never pass silently.
+30.Unless explicitly silenced.
+31.In the face of ambiguity, refuse the temptation to guess.
+32.There should be one-- and preferably only one --obvious way to do it.
+33.Although that way may not be obvious at first unless you're Dutch.
+34.Now is better than never.
+35.Although never is often better than *right* now.
+36.If the implementation is hard to explain, it's a bad idea.
+37.If the implementation is easy to explain, it may be a good idea.
+38.Namespaces are one honking great idea -- let's do more of those!Beautiful is better than ugly.
+39.Explicit is better than implicit.
+40.Simple is better than complex.
+41.Complex is better than complicated.
+42.Flat is better than nested.
+43.Sparse is better than dense.
+44.Readability counts.
+45.Special cases aren't special enough to break the rules.
+46.Although practicality beats purity.
+47.Errors should never pass silently.
+48.Unless explicitly silenced.
+49.In the face of ambiguity, refuse the temptation to guess.
+50.There should be one-- and preferably only one --obvious way to do it.
+51.Although that way may not be obvious at first unless you're Dutch.
+52.Now is better than never.
+53.Although never is often better than *right* now.
+54.If the implementation is hard to explain, it's a bad idea.
+55.If the implementation is easy to explain, it may be a good idea.
+56.Namespaces are one honking great idea -- let's do more of those!"""
+
+
+# 1.1 Counting specific words
+words_to_count = ["better", "never", "is"]
+counter_better = 0
+counter_never = 0
+counter_is = 0
+
+
+for words_to_count[0] in zen_of_python:
+    counter_better += 1
+for words_to_count[1] in zen_of_python:
+    counter_never += 1
+for words_to_count[2] in zen_of_python:
+    counter_is += 1
+
+
+print(f"Word 'better' occurred {counter_better} times.")
+print(f"Word 'never' occurred {counter_never} times.")
+print(f"Word 'is' occurred {counter_is} times.")
+print("=====================================")
+
+
+# 1.2 Displaying all text in uppercase
+zen_of_python_uppercase = zen_of_python.upper()
+print(zen_of_python_uppercase)
+print("=====================================")
+
+
+# 1.3 Replacing "i" with "&"
+zen_with_replaced_and = zen_of_python.replace("and", "&")
+print(zen_with_replaced_and)
+print("=====================================")
