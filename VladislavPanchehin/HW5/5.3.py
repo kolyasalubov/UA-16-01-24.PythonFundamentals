f2401098d1a471bf187ee5ee980f2bfc4d852ef4
Vladislav Panchehin <vaduamediafox@gmail.com>
2024-02-01 16:14:49 +0200

    added 5.3.py

---
+++ VladislavPanchehin/HW5/5.3.py
@@ -0,0 +1,21 @@
+﻿
+# variant 1
+# num = input("Entered your number: ")
+# factorial = 1
+
+
+# while 1 < num:
+#  factorial *= num
+#  num -= 1
+# print(factorial)
+
+# # variant 2
+n = int(input("Entered your number: "))
+f = 1
+if isinstance(n,int) and 0 <=0:
+ for item in range(2, n+1):
+    f *= item
+    print (f"{item}! {f}")
+
+ else:
+    print ("Number is not intenger")    
