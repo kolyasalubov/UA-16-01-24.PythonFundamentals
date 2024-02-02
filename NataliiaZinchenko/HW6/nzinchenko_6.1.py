e5ef7f5b9517ccf5708ac652da22a8a8fc460e97
NataliiaZinchenko <nataellie@gmail.com>
2024-02-02 15:12:32 +0100

    minor improvements in 6.2

--- NataliiaZinchenko/HW6/nzinchenko_6.1.py
+++ NataliiaZinchenko/HW6/nzinchenko_6.1.py
@@ -2,6 +2,7 @@ even_list = []
 odd_list = []
 exceptions_list = []
 
+
 for i in range(1,11):
     if i % 2 == 0:
         even_list.append(i)
@@ -9,6 +10,7 @@ for i in range(1,11):
         odd_list.append(i)
     elif i % 2 != 0 and i % 3 != 0:
         exceptions_list.append(i)
+
 
 print(f"Even numbers: {even_list}")
 print(f"Odd numbers: {odd_list}")
