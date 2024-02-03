ef1be1cb4757f4828826cab2f43ce215472fbea8
Olexandr_Hramatovych <sashahram08@gmail.com>
2024-02-03 13:55:40 +0200

    commenting the first task

--- OlexanderHramatovych/HW6/6-1.py
+++ OlexanderHramatovych/HW6/6-1.py
@@ -1,14 +1,16 @@
-list_of_even_numbers = []
-list_of_odd_numbers = []
-no_lists_numbers = []
+list_of_even_numbers = [] #creating list for putting even numbers
+list_of_odd_numbers = []  #creating list for putting odd numbers which are divisivle by 3
+no_lists_numbers = []     #creating list for putting numbers which are divisivle  neither by 2  nor by 3
 
-for number in range(1,10+1):
+for number in range(1,10+1): # loop for the numbers
     if number % 2 == 0:
-        list_of_even_numbers.append(number)
+        list_of_even_numbers.append(number) # appending into the list even 
     elif number % 3 == 0:
-        list_of_odd_numbers.append(number)
+        list_of_odd_numbers.append(number) # appending into the list odd 
     else:
-        no_lists_numbers.append(number)
+        no_lists_numbers.append(number) # appending into the list other numbers
+
+# outputting the resuly
 print(f"Even numbers in range from 1 to 10 are: {list_of_even_numbers}")
 print(f"Odd numbers which are divisible by 3 in range from 1 to 10 are: {list_of_odd_numbers}")
 print(f"Numbers which are  divisible neither by 2 nor 3 in range from 1 to 10: {no_lists_numbers}")
