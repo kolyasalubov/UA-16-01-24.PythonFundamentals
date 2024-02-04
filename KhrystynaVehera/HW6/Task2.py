9549be96843d4328a8d129efbc8402aeffa0a496
Khrystyna <kristina.hlyvnuk@gmail.com>
2024-02-05 01:14:38 +0200

    Add my Homework6

---
+++ KhrystynaVehera/HW6/Task2.py
@@ -0,0 +1,7 @@
+while True:
+    user_login = input("Enter you login: ")
+    if user_login == "First":
+        print(f"Greetings {user_login}!")
+        break
+    else:
+        print(f"Eror, this user name {user_login} is not in database. Please try again or sign up.")
