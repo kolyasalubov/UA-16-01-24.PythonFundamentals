0ec9b49357d5d7ceec8db15f8dafab4aeec1c862
kolyasalubov <36229492+kolyasalubov@users.noreply.github.com>
2024-02-05 13:31:29 +0200

    Update task3.py

--- PolinaChernyshova/HW3/task3.py
+++ PolinaChernyshova/HW3/task3.py
@@ -1,5 +1,5 @@
 # Interchange the values of two variables without using the third variable.
-
+Koliasa Liubov: good job
 first_num = -12
 second_num = 65
 print(f'first_num = {first_num}, second_num = {second_num} ')
