804d2e4b3efb37ae3e8a7f3779d40f427ebbc806
kolyasalubov <36229492+kolyasalubov@users.noreply.github.com>
2024-02-05 13:31:59 +0200

    Update task2.py

--- PolinaChernyshova/HW3/task2.py
+++ PolinaChernyshova/HW3/task2.py
@@ -18,7 +18,7 @@ print(f'Reversed number = {reverse_number}')
 print(f'Sorted number = {sorted_number}')
 
 #I solved it this way, before realizing that we had worked without a loop
-excellent work!!!
+Koliasa Liubov: excellent work!!!
 
 # sum_of_numbers = 0 
 # reverse_number = ''
