29ab4b8ccfc4b407dfef07f613cddf622c34de66
PavloKandur <pavlokandur@gmail.com>
2024-02-07 13:12:16 +0200

    changed hw6 task2

--- PavloKandur/HW6/task2.py
+++ PavloKandur/HW6/task2.py
@@ -1,7 +1,7 @@
 while(True):
     user_login = input("Please enter your login: ")
     if user_login == "First":
-        print(f"Greetings {user_login}")
+        print(f"Greetings, {user_login}")
         break
     else:
         print("Login is wrong, try again")
