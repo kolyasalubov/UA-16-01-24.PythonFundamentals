29886d73cd5a32058706dd51f4ceae2534f38042
NataliiaZinchenko <nataellie@gmail.com>
2024-02-07 19:20:25 +0100

    added calculate_rectangle_area_console function

--- NataliiaZinchenko/HW7/nzinchenko_7.2.py
+++ NataliiaZinchenko/HW7/nzinchenko_7.2.py
@@ -1,13 +1,33 @@
 # Writing 3 programs which calculate area of a rectangle, triangle and circle
 
 
-def calculating_rectangle_area(length, width):
+def calculate_rectangle_area(length, width):
     '''Function that calculates rectangle area using length and width
        length: int OR float
        width: int OR float
     '''
     rectangle_area = length * width
     return rectangle_area
+
+
+def calculate_rectangle_area_console():
+    '''Function that printing out the result of rectangle area calculation,
+       depending on the data type of rectangle length & width (int / float)
+    '''
+    print("Ok! Let's calculate rectangle area!\n")
+    custom_rectangle_length = float(input("Enter rectangle length: \n"))
+    custom_rectangle_width = float(input("Enter rectangle width: \n"))
+
+
+    output_rectangle_area = calculate_rectangle_area(custom_rectangle_length, custom_rectangle_width)
+    
+
+    if output_rectangle_area - int(output_rectangle_area) > 0:
+        print(f"Rectangle area is: {output_rectangle_area:.2f}")
+    else:
+        output_rectangle_area = int(output_rectangle_area)
+        print(f"Rectangle area is: {output_rectangle_area}")
+
 
 
 def calculating_triangle_area(base, height):
@@ -30,25 +50,7 @@ def calculating_circle_area(radius):
 
 choosing_object = input("Which object's area you want to calculate (Rectangle, triangle or circle): \n").lower()
 if choosing_object == "rectangle":
-    print("Ok! Let's calculate rectangle area!\n")
-    custom_rectangle_length = input("Enter rectangle length: \n")
-    custom_rectangle_width = input("Enter rectangle width: \n")
-
-
-    if "." in custom_rectangle_width and custom_rectangle_length:
-        custom_rectangle_length = float(custom_rectangle_length)
-        custom_rectangle_width = float(custom_rectangle_width)
-    else:
-        custom_rectangle_length = int(custom_rectangle_length)
-        custom_rectangle_width = int(custom_rectangle_width)
-
-    output_rectangle_area = calculating_rectangle_area(custom_rectangle_length, custom_rectangle_width)
-    
-    if type(custom_rectangle_length) and type(custom_rectangle_width) == float:
-        print(f"Rectangle area is: {output_rectangle_area:.2f}")
-    else:
-        print(f"Rectangle area is: {output_rectangle_area}")
-
+    calculate_rectangle_area_console()
 
 #elif choosing_object == "triangle":
 #    print("Ok! Let's calculate triangle area!\n")
