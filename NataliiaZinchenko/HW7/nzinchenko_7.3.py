bd257794c7754635ba9261ca2339130885aa36b2
NataliiaZinchenko <nataellie@gmail.com>
2024-02-08 14:30:35 +0100

    solved 7.3

--- NataliiaZinchenko/HW7/nzinchenko_7.3.py
+++ NataliiaZinchenko/HW7/nzinchenko_7.3.py
@@ -5,19 +5,16 @@ def calculate_number_of_characters(input_string):
        input_string: str
     '''
     letters_list = []
-    counting_list = []
     counting_dict = {}
     for letter in input_string:
         letters_list.append(letter)
-    
+
+
     for letters_list_element in letters_list:
-        counter = 0
-        if letters_list_element not in letters_list:
-            counter = 1
-            counting_dict.update(f"{letters_list_element}: {counter}")
-        else:
-            counter += 1
-            counting_dict.update(f"{letters_list_element}: {counter}")
+        if letters_list_element not in counting_dict.keys():
+            counting_dict[letters_list_element] = 1
+        elif letters_list_element in counting_dict.keys():
+            counting_dict.update({letters_list_element: counting_dict[letters_list_element] + 1}) 
 
 
     return counting_dict
