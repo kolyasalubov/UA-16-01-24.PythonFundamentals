f5b72366d81cc6ce15818c7da7b24d8f543afb0b
Eneyif <157028775+Eneyif@users.noreply.github.com>
2024-02-12 01:56:33 +0200

    Delete KaterynaKasapchuk/HW5 directory

--- KaterynaKasapchuk/HW5/Task1.py
+++
@@ -1,4 +0,0 @@
-numbers = [5,2,7,9,3,1]
-
-for number in numbers:
-    number = float(number)
--- KaterynaKasapchuk/HW5/task3.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Write a number"))
-
-number = 1
-while n > 0:
-    number *= n
-    n -= 1
-print (number)
