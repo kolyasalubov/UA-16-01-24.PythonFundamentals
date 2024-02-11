de67ddc5beac89b7189567f3d1cdc7bfa1ef2b38
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-02-11 11:50:22 +0200

    added comments into dirst task

--- OlexanderHramatovych/HW7/Task1.py
+++ OlexanderHramatovych/HW7/Task1.py
@@ -1,5 +1,6 @@
+#creating the function which definds which number is largest
 def largest(num1,num2):
-    if num1 > num2:
+    if num1 > num2: # creating the conditions
         return num1
     elif num2 > num1:
         return num2
