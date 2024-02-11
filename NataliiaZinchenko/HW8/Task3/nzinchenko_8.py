8b85470f4d8e45ebf0d0a039d2f13e6957825bfc
NataliiaZinchenko <nataellie@gmail.com>
2024-02-11 16:25:48 +0100

    added validation and refactoring

--- NataliiaZinchenko/HW8/Task3/nzinchenko_8.py
+++ NataliiaZinchenko/HW8/Task3/nzinchenko_8.py
@@ -1,5 +1,37 @@
 from math import pow, pi
 
+def validation_and_conversion(num):
+    '''Function that validates if entered value is:
+       1. Is not negative
+       2. Is numeric
+       3. Is not 0
+       4. Either float or int
+    '''
+    if "-" in num:
+        raise Exception("Entered value can't be less than 0")
+    
+    if not num.replace(".", "").isdigit():
+        raise Exception("Entered value is not numeric")
+
+    if "." in num:
+       num = float(num)
+    else:
+       num =  int(num)
+
+    if num <= 0:
+        raise Exception("Entered value can't be 0")
+    
+    return num
+
+
+def pretty_print(num):
+    '''Returns either float rounded up to 2 decimals digits
+       or returns int without decimals digits
+    '''
+    if float(num) - int(num) > 0:
+        return f"{num:.2f}"
+    
+    return int(num)
 
 def rectangle_area_calculation(length, width):
     '''Function that calculates area of a rectangle by formula: 
@@ -29,3 +61,4 @@ def circle_area_calculation(radius):
     '''
     circle_area = pi * pow(radius, 2)
     return circle_area
+    
