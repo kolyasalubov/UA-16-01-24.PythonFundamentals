ebcc02069ef2ad9de04af0a31c2c78d0fa3336f3
PolinaCh14 <chernyshovapolina014@gmail.com>
2024-02-11 12:48:55 +0200

    added folder HW8 with homework № 3

---
+++ PolinaChernyshova/WH8/task_3_calculate_area.py
@@ -0,0 +1,22 @@
+import math
+
+def calculate_area_rectangle(side_a: float, side_b: float )->float:
+    '''
+    The function takes to float type arguments: side_a, side_a
+    Return the area of a rectangle in float type 
+    '''
+    return side_a * side_b
+
+def calculate_area_triangle(side_a: float, height: float )->float:
+    '''
+    The function takes to float type arguments: side_a, height
+    Return the area of a triangle in float type 
+    '''
+    return 0.5 * side_a * height
+
+def calculate_area_circle(radius: float)->float:
+    '''
+    The function takes to float type argument: radius
+    Return the area of a circle in float type 
+    '''
+    return math.pi * math.pow(radius,2)
