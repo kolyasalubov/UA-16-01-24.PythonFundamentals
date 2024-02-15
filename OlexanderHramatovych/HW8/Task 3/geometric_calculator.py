dbc5213d360194f8f0a734d5d808a5c9b6c7f5ca
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-02-15 17:35:55 +0200

    wrote functions

---
+++ OlexanderHramatovych/HW8/Task 3/geometric_calculator.py
@@ -0,0 +1,11 @@
+import math
+from math import pow
+from math import pi 
+
+def rectangle_area(first_side_of_rectangle,second_side_of_rectangle):
+    return first_side_of_rectangle * second_side_of_rectangle
+def triangle_area(hight_of_triangle:float,cathetus:float)->float:
+    return 0.5 * hight_of_triangle * cathetus
+def circle_area(radius):
+    return(radius ** 2) * pi
+
