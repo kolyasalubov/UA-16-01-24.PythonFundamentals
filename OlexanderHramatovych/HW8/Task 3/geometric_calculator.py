10675a6943124737e08508b998a78ccf36141c05
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-02-15 17:50:33 +0200

    adding float

--- OlexanderHramatovych/HW8/Task 3/geometric_calculator.py
+++ OlexanderHramatovych/HW8/Task 3/geometric_calculator.py
@@ -2,7 +2,7 @@ import math
 from math import pow
 from math import pi 
 
-def rectangle_area(first_side_of_rectangle,second_side_of_rectangle):
+def rectangle_area(first_side_of_rectangle:float,second_side_of_rectangle:float)->float:
     return first_side_of_rectangle * second_side_of_rectangle
 def triangle_area(hight_of_triangle:float,cathetus:float)->float:
     return 0.5 * hight_of_triangle * cathetus
