cfe7abb2434d672d5f2d78c88bca52d70fd76a65
NataliiaZinchenko <nataellie@gmail.com>
2024-02-16 11:51:42 +0100

    added validation of length

--- NataliiaZinchenko/HW8/Task2/nzinchenko8_2.py
+++ NataliiaZinchenko/HW8/Task2/nzinchenko8_2.py
@@ -1,4 +1,5 @@
 # Creating a program which checks if user entered valid password
+import re
 
 def is_password_valid(input_string): 
     '''Function checks if entered password is valid
@@ -15,4 +16,20 @@ def is_password_valid(input_string):
        returns False: if entered password invalid
     '''
 
+    pattern_az = "[a-z]"
+    pattern_AZ = "[A-Z]"
+    pattern_09 = "[0-9]"
+    pattern_special_chars = "[$#@]"
+
     
+    if len(input_string) < 6:
+        print("Entered password can't be less than 6 characters. Enter another password.")
+    
+    if len(input_string) > 17:
+        print("Entered password can't be more than 16 characters. Enter another password.")
+
+    
+
+
+
+
