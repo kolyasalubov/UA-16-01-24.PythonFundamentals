f40acfc4b4895e67023331c911bf392b46b39217
NataliiaZinchenko <nataellie@gmail.com>
2024-02-18 18:30:13 +0100

    edited task9_1

--- NataliiaZinchenko/HW9/Task9_1/nzinchenko9_1.py
+++ NataliiaZinchenko/HW9/Task9_1/nzinchenko9_1.py
@@ -9,29 +9,27 @@ def guess_the_number(num):
     '''
     num = int(num)
     random_number = randint(1,100)
-    attempt = 0
+    attempt = 1
     while attempt <= 10:
         if num == random_number:
-            attempt += 1
-            print(f"You guessed random generated number {random_number}")
-            return "string"
+            return f"You guessed random generated number {random_number}"
 
 
         elif num < random_number:
-            attempt += 1
             print(f"This is your attempt {attempt} from 10")
             try_again = int(input("Your number is less than random generated one. Try again: \n"))
             num = try_again
-            #print(f"Your attempt is {attempt} and your number is {num}")
-            #print(f"Correct answer is: {random_number}")
+
+
         elif num > random_number:
-            attempt += 1
             print(f"This is your attempt {attempt} from 10")
             try_again = int(input("Your number is bigger than random generated one. Try again: \n"))
             num = try_again
-            #print(f"Your attempt is {attempt} and your number is {num}")
-            #print(f"Correct answer is: {random_number}")
+
+        attempt += 1
+        print(f"Correct: {random_number}")
+    return "Out of guesses"
 
 
 user_guesses_number = input("Guess the random number from 1 to 100 and enter it here: \n")
-guess_the_number(user_guesses_number)
+print(guess_the_number(user_guesses_number))
