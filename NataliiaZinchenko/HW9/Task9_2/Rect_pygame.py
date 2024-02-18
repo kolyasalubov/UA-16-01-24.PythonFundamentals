17f8464461c3bc2cb9199c71e4f6c65562ac586f
NataliiaZinchenko <nataellie@gmail.com>
2024-02-18 18:05:06 +0100

    added limitations to the right and down movements

--- NataliiaZinchenko/HW9/Task9_2/Rect_pygame.py
+++ NataliiaZinchenko/HW9/Task9_2/Rect_pygame.py
@@ -7,12 +7,12 @@ HEIGHT_DISPLAY=500
 
 COORD_X=50
 COORD_Y=50
-WIDTH_RECTANGLE=40
-HEIGHT_RECTANGLE=60
+WIDTH_RECTANGLE=50
+HEIGHT_RECTANGLE=50
 DELTA_STEP=5
 
-BLACK_COLOR = (0, 0, 0)
-RED_COLOR = (250, 0, 0)
+WHITE_COLOR = (255, 255, 255)
+BLUE_COLOR = (0, 0, 255)
 
 pygame.init()
 
@@ -35,18 +35,18 @@ while run:
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT] and COORD_X > DELTA_STEP:
-        COORD_X = COORD_X-DELTA_STEP
-    if keys[pygame.K_RIGHT]:
-        COORD_X = COORD_X+DELTA_STEP
+        COORD_X = COORD_X - DELTA_STEP
+    if keys[pygame.K_RIGHT] and COORD_X < WIDTH_DISPLAY - DELTA_STEP - WIDTH_RECTANGLE:
+        COORD_X = COORD_X + DELTA_STEP
     if keys[pygame.K_UP] and COORD_Y > DELTA_STEP:
-        COORD_Y = COORD_Y-DELTA_STEP
-    if keys[pygame.K_DOWN]:
-        COORD_Y = COORD_Y+DELTA_STEP
+        COORD_Y = COORD_Y - DELTA_STEP
+    if keys[pygame.K_DOWN] and COORD_Y < HEIGHT_DISPLAY - DELTA_STEP - HEIGHT_RECTANGLE:
+        COORD_Y = COORD_Y + DELTA_STEP
 
 
-    gameDisplay.fill(BLACK_COLOR) 
+    gameDisplay.fill(WHITE_COLOR) 
 
-    pygame.draw.rect(gameDisplay, RED_COLOR, [COORD_X, 
+    pygame.draw.rect(gameDisplay, BLUE_COLOR, [COORD_X, 
                                               COORD_Y, 
                                               WIDTH_RECTANGLE, 
                                               HEIGHT_RECTANGLE])
