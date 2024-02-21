35cc1831f37ce0a39eb021a475623b956d2a361b
Vladislav Panchehin <vaduamediafox@gmail.com>
2024-02-22 00:14:54 +0200

    added HW10 files 10.1,10.2,10,3

---
+++ VladislavPanchehin/HW10/10.2.py
@@ -0,0 +1,25 @@
+﻿class Human:
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        
+    def introduce(self):
+        print(f"Hello! My name is {self.name}, I am old {self.age}, and I'm a man {self.gender}")
+    #  Create a class method in class
+    @classmethod
+    def greeting_info(cls):
+        return f"This is a member of the Homo sapiens species 'Class name - {cls.__name__}'"
+    # Create class static method
+    @staticmethod
+    def greetings_user():
+        return "Burn,Baby, Burn!"
+        
+# Create class object Human
+person_one = Human("Vladislav",30,"man")
+#run method
+person_one.introduce()
+#  Calling a method class
+print(Human.greeting_info())
+# Calling a static method
+print(Human.greetings_user())
---
+++ VladislavPanchehin/HW10/10.3.py
@@ -0,0 +1,54 @@
+﻿
+# Creare class Employee
+class Employee:
+    """Class with information about employees and their salaries
+    """
+    # Create starter counter Employee
+    COUNT_EMPLOYEE = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+# Counter increase
+        Employee.COUNT_EMPLOYEE += 1
+
+    def employee_info(self):
+        return f'Employee {self.name} receives a salary {self.salary}$'
+
+    @classmethod
+    def employee_total(cls):
+        return '\n'f'Total employees {cls.COUNT_EMPLOYEE} and Total Salarys All Employees'
+# display information about the base classes from which the employee class
+
+
+class About_base_classes(Employee):
+    """Information about base classes
+    """
+    @staticmethod
+    def base_class_information():
+        print('\n'"Base classes:", Employee.__base__, '\n')
+        print("Class namespace:", Employee.__dict__, '\n')
+        print("Class name:", Employee.__name__, '\n')
+        print("Module name:", Employee.__module__, '\n')
+        print("Documentation:", Employee.__doc__)
+
+
+# Test
+Employee_1 = Employee("Vika", 3000)
+Employee_2 = Employee("Oleg", 100)
+Employee_3 = Employee("Serg", 30000)
+Employee_4 = Employee("Andrew", 2000)
+Employee_5 = Employee("Max", 232)
+
+# Info about Employee
+print(Employee_1.employee_info())
+print(Employee_2.employee_info())
+print(Employee_3.employee_info())
+print(Employee_4.employee_info())
+print(Employee_5.employee_info())
+
+# Total Employee
+print(Employee.employee_total())
+#print()
+#
+About_base_classes.base_class_information()
