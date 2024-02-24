de3f9ac4bd6234c5b671af71b8a97463ed4bdc5a
Vladislav Panchehin <vaduamediafox@gmail.com>
2024-02-24 21:22:05 +0200

    added hw10.2

--- VladislavPanchehin/HW10/10.2/10.2.2.py
+++ VladislavPanchehin/HW10/10.2/10.2.2.py
@@ -1,1 +1,7 @@
-﻿
+﻿import random
+
+
+class Ghost:
+    def __init__(self):
+        self.colors = ["white", "yellow", "purple", "red"]
+        self.color = random.choice(self.colors)
---
+++ VladislavPanchehin/HW10/10.2/10.2.5.py
@@ -0,0 +1,31 @@
+﻿import math
+class Sphere(object):
+    def __init__(self,radius,mass):
+        self.radius = radius
+        self.mass = mass
+#Radius of the sphere.
+    def get_radius(self):
+        return self.radius
+#Mass of the sphere
+    def get_mass(self):
+        return self.mass
+#Volume of the sphere          
+    def get_volume(self):
+        return round((4/3) * math.pi * pow(self.radius,3))
+#Surface of the sphere    
+    def get_surface_area(self):
+        return round(4 * math.pi * pow(self.radius,2))
+#mass of the sphere
+    def get_density(self):
+         return round(self.mass/self.get_volume())
+    
+#tested
+
+ball = Sphere(2, 50)
+print(ball.get_radius())         
+print(ball.get_mass())           
+print(ball.get_volume())         
+print(ball.get_surface_area())   
+print(ball.get_density())        
+
+    
