91fd1706cb07d5a00c1a1bda239ea03c364d6c45
Vladislav Panchehin <vaduamediafox@gmail.com>
2024-02-25 14:53:00 +0200

    added HW11

---
+++ VladislavPanchehin/HW11/11.1.py
@@ -0,0 +1,33 @@
+﻿# Create function and dict days of week
+
+def day_of_week(num):
+    days_of_week = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sanday'
+    }
+
+    try:
+
+        num = int(num)
+        if num < 0:
+            return "Enter a number greater than 0."
+
+        elif num <= 7:
+            return days_of_week[num]
+
+        else:
+            return "There are 7 days in a week"
+
+    except:
+        ValueError
+    return "Enter a valid number!"
+
+
+#
+user_number = input('Enter your number: ')
+print(day_of_week(user_number))
---
+++ VladislavPanchehin/HW11/11.2.py
@@ -0,0 +1,22 @@
+﻿# Main function
+def age_even_or_odd(user_age, user_name):
+    if user_age < 0:
+        raise ValueError("ERROR! - The entered number is negative!")
+
+    if user_age % 2 == 0:
+        return (f"'{user_name}' your Age: '{user_age}' is even")
+    else:
+        return (f" User:'{user_name}' your Age: '{user_age}' is odd")
+
+# Global logic programm. Input age and name
+def main():
+    try:
+        user_name = (input("User enter your name: "))
+        user_age = int(input("Enter your age: "))
+        result = age_even_or_odd(user_age, user_name)
+        print(result)
+    except ValueError as e:
+        print(e)
+
+# Run programm
+run = main()
