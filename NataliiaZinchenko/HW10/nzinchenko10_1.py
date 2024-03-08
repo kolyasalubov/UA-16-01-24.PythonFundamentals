4dedd15830cd258ba42226d8e378811fb9a25830
NataliiaZinchenko <nataellie@gmail.com>
2024-03-08 20:32:01 +0000

    solved 10.1

--- NataliiaZinchenko/HW10/nzinchenko10_1.py
+++ NataliiaZinchenko/HW10/nzinchenko10_1.py
@@ -1,19 +1,19 @@
 # Creating a polygon class and a rectangle class and finding square of rectangle
 
-class Polygon():
+class Polygon:
     '''
-    Class defining Polygon objects
+    Class Polygon with base and height properties and area() method
     '''
-    def __init__(self, square = 0, area = 0):
-        self.area = area
-        self.square = square
-
-
-
-class Rectangle():
-    '''
-    Class defining Rectangle objects
-    '''
-    def __init__(self, width = 0, height = 0):
-        self.width = width
+    def __init__(self, base, height):
+        self.base = base
         self.height = height
+
+    def area(self):
+        return self.base * self.height
+    
+class Rectangle(Polygon):
+    '''
+    Class Rectangle which inherits properties from Polygon class
+    '''
+    def __init__(self, base, height):
+        super().__init__(base, height)
