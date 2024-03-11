b2445fa03b42444a03cff925071bc63bc5697826
NataliiaZinchenko <nataellie@gmail.com>
2024-03-11 18:09:00 +0000

    added tasks descriptions as comments

---
+++ NataliiaZinchenko/HW11/nzinchenko11_1.py
@@ -0,0 +1,5 @@
+# Write a program that prompts the user to enter their age, and then displays a
+# message stating whether the age is even or odd. The program must provide the ability
+# to enter a negative number, and in this case generate an exception. The master code 
+# should call a function that processes the information entered.
+
---
+++ NataliiaZinchenko/HW11/nzinchenko11_2.py
@@ -0,0 +1,4 @@
+# Write a program that analyzes the entered number and, depending on the number,
+# gives the day of week that corresponds to this number (1 is Monday, 2 is Tuesday etc).
+# Take into account cases of entering numbers from 8 and more, as well as cases of
+# entering non-numerical data.
