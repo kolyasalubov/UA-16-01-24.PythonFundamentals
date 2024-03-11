50814a624d424a9eee13da909abbd41db7bd81d8
NataliiaZinchenko <nataellie@gmail.com>
2024-03-11 18:31:10 +0000

    11_1: solved task

--- NataliiaZinchenko/HW11/nzinchenko11_1.py
+++ NataliiaZinchenko/HW11/nzinchenko11_1.py
@@ -4,15 +4,32 @@
 # should call a function that processes the information entered.
 
 class CustomError(Exception):
+    '''
+    CustomError class to raise and catch custom exceptions
+    '''
     def __init__(self, data):
         self.data = data
 
     def __str__(self):
         return repr(self.data)
 
-def is_age_even_or_odd(age):
-        if age % 2 == 0:
-            print(f"Entered age {age} is even")
-        else:
-            print(f"Entered age {age} is odd")
 
+def is_age_even_or_odd(age):
+    if age % 2 == 0:
+        return f"Entered age {age} is even"
+    else:
+        return f"Entered age {age} is odd"
+
+
+try:
+    get_user_age = int(input("Enter your age to check if it's even or odd:\n"))
+    if get_user_age < 0:
+        raise CustomError(f"Entered age is {get_user_age}: can't be negative")
+    elif get_user_age == 0:
+        raise CustomError(f"Entered age is {get_user_age}: can't be 0")
+    elif get_user_age > 105:
+        raise CustomError(f"Entered age is {get_user_age}: can't be more than 105")
+    print(is_age_even_or_odd(get_user_age))
+except (ValueError, CustomError) as e:
+    print(f"Exception: {e}")
+    
