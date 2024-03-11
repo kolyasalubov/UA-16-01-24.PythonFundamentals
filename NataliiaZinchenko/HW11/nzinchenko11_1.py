a539651d9e69b9be6b5f4190cb2d4d2bb410bc1a
NataliiaZinchenko <nataellie@gmail.com>
2024-03-11 18:12:42 +0000

    11_1: created CustomError

--- NataliiaZinchenko/HW11/nzinchenko11_1.py
+++ NataliiaZinchenko/HW11/nzinchenko11_1.py
@@ -3,3 +3,16 @@
 # to enter a negative number, and in this case generate an exception. The master code 
 # should call a function that processes the information entered.
 
+class CustomError(Exception):
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return repr(self.data)
+
+def is_age_even_or_odd(age):
+        if age % 2 == 0:
+            print(f"Entered age {age} is even")
+        else:
+            print(f"Entered age {age} is odd")
+
